5a1e2b1c30a95542566f2fd002cc572a666bdcb4
akhudiakov97 <akhudiakov97@gmail.com>
2025-02-20 16:43:23 +0000

    final version

--- 3-mfe/web-app/next.config.mjs
+++ 3-mfe/web-app/next.config.mjs
@@ -1,23 +1,4 @@
 /** @type {import('next').NextConfig} */
-import { NextFederationPlugin } from "@module-federation/nextjs-mf";
+const nextConfig = {};
 
-const nextConfig = {
-    webpack(config, options) {
-        if (!options.isServer) {
-            config.plugins.push(
-                new NextFederationPlugin({
-                    name: "host",
-                    remotes: {
-                        movies: "movies@http://localhost:5001/assets/moviesRemoteEntry.js",
-                    },
-                    filename: 'assets/moviesRemoteEntry.js',
-                })
-            );
-        }
-
-        return config;
-    },
-};
-
-
-export default nextConfig; // Export the configuration object
+export default nextConfig;
